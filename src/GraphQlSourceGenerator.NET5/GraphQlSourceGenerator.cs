fa56213bf809c94f8a2ac35af76aca1dfcb396da
Shkarin <shkarin@protei.ru>
2021-11-23 13:03:56 +0300

    Added ISourceGenerator based project

---
+++ src/GraphQlSourceGenerator.NET5/GraphQlSourceGenerator.cs
@@ -0,0 +1,60 @@
+﻿using GraphQlSourceGenerator.NET5.Utils;
+using GraphQlSourceGenerator.Services;
+using GraphQlSourceGenerator.Utils;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using GraphQlSourceGenerator.Models;
+
+namespace GraphQlSourceGenerator.NET5
+{
+    [Generator]
+    public class GraphQlSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+#if false
+            _ = System.Diagnostics.Debugger.Launch();
+#endif
+
+            try
+            {
+                if (context.IsValidReferences()
+                 && context.AdditionalFiles.TryParse<GraphQlGeneratorSettings>("GraphQlGeneratorSettings.json", context, context.CancellationToken, out var graphQlGeneratorSettings)
+                 && context.AdditionalFiles.TryParse<GraphQlSchema>("schema.json", context, context.CancellationToken, out var schema))
+                {
+                    context.CancellationToken.ThrowIfCancellationRequested();
+
+                    var generator = new Generator(schema, graphQlGeneratorSettings, nameof(GraphQlSourceGenerator));
+
+                    context.CancellationToken.ThrowIfCancellationRequested();
+
+                    IList<(string FileName, string FileContent)> filesInfo = generator.Generate();
+
+                    context.CancellationToken.ThrowIfCancellationRequested();
+
+                    foreach (var (fileName, fileContent) in filesInfo)
+                    {
+                        context.AddSource(fileName, SourceText.From(fileContent, Encoding.UTF8));
+                    }
+                }
+            }
+            catch (OperationCanceledException _)
+            {
+                //ignored
+            }
+            catch (Exception e)
+            {
+                context.AddExceptionReport(e);
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
---
+++ src/GraphQlSourceGenerator.NET5/Utils/GeneratorSettingsHelper.cs
@@ -0,0 +1,53 @@
+﻿using GraphQlSourceGenerator.Models;
+using Microsoft.CodeAnalysis;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace GraphQlSourceGenerator.NET5.Utils
+{
+    internal static class GeneratorSettingsHelper
+    {
+        public static bool TryParse<TOut>(this ImmutableArray<AdditionalText> additionalFiles,
+                                          string fileName,
+                                          GeneratorExecutionContext context,
+                                          CancellationToken token,
+                                          out TOut output)
+            where TOut : class
+        {
+            output = null;
+            var settingsAdditionalFile = additionalFiles.FirstOrDefault(af => af.Path.Contains(fileName));
+
+            if (settingsAdditionalFile != null)
+            {
+                var text = settingsAdditionalFile.GetText(token);
+                try
+                {
+                    var serializerSettings = new JsonSerializerSettings
+                    {
+                        Converters = { new StringEnumConverter() }
+                    };
+                    var settings = JsonConvert.DeserializeObject<TOut>(text?.ToString(), serializerSettings);
+                    if (settings != null)
+                    {
+                        output = settings;
+                        return true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    context.AddExceptionReport(e);
+                }
+            }
+            else
+            {
+                context.AddIoReport(fileName);
+            }
+
+            return false;
+        }
+    }
+}
