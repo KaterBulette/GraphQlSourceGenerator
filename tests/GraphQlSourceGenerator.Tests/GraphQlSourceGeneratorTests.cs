57a7eb2b9dd7f7d5b48ea2fb3e53ab2b974e1feb
Shkarin <shkarin@protei.ru>
2021-11-23 13:16:56 +0300

    Added tests project

---
+++ tests/GraphQlSourceGenerator.Tests/GraphQlSourceGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace GraphQlSourceGenerator.Tests
+{
+    public class GraphQlSourceGeneratorTests
+    {
+        [Fact]
+        public void GraphQlSourceGeneratorReturnFalseIfSchemaFileNotExist()
+        {
+            var target = CreateTarget();
+            target.SchemaPath = "../schema.json";
+
+            var result = target.Execute();
+
+            Assert.False(result);
+        }
+
+        private GraphQlSourceGenerator CreateTarget()
+        {
+            return new();
+        }
+    }
+}
